59c0991192242410db50c7c344c16a1df44b0088
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2022-05-30 17:46:55 +0530

    Setting up the Adapter class commit

--- app/src/main/java/com/example/recyclerviewtutorial/MovieAdapter.java
+++ app/src/main/java/com/example/recyclerviewtutorial/MovieAdapter.java
@@ -1,5 +1,7 @@
 package com.example.recyclerviewtutorial;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -8,24 +10,42 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MyViewHolder> {
+    Context context;
+    ArrayList<ModelClass> modelClass;
+
+    public MovieAdapter(Context context , ArrayList<ModelClass> modelClass){
+        this.context = context;
+        this.modelClass = modelClass;
+    }
+
     @NonNull
     @Override
     public MovieAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         // This is where we are going to inflate the layout (Basically giving a look to the rows)
-        return null;
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.recycler_view_row, parent, false);
+
+        return new MovieAdapter.MyViewHolder(view); // instead of returning null, return a new MovieAdapter
     }
 
     @Override
     public void onBindViewHolder(@NonNull MovieAdapter.MyViewHolder holder, int position) {
         // assigning values to the views we created in the recycler_view_row layout file
         // based on the position of the recycler view
+
+        holder.name.setText(modelClass.get(position).getName());
+        holder.rating.setText(modelClass.get(position).getRating());
+        holder.imageView.setImageResource(modelClass.get(position).getImage());
     }
 
     @Override
     public int getItemCount() {
         // the recycler view just wants to know the number of items you want to be displayed
-        return 0;
+        return modelClass.size();
     }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder{
