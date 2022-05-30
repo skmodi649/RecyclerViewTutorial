923917fd6dab06071bbd24f0e2f65205cb0209e7
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2022-05-30 17:58:47 +0530

    Setting up the Adapter class commit

--- app/src/main/java/com/example/recyclerviewtutorial/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewtutorial/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.recyclerviewtutorial;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -12,13 +14,24 @@ public class MainActivity extends AppCompatActivity {
     String[] Movie_names = {"RRR" , "cinema" , "matrix" , "pushpa" , "war"};
     String[] Movie_rating = {"6.0" , "5.0" , "9.0" , "8.0" , "10.0"};
 
-    int[] MovieImages = {R.drawable.cinema , R.drawable.cinema , R.drawable.matrix , R.drawable.pushpa , R.drawable.war};
+    int[] MovieImages = {R.drawable.rrr , R.drawable.cinema , R.drawable.matrix , R.drawable.pushpa , R.drawable.war};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Attaching the adapter to the recycler view
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+
         setUpModel();
+
+        // Create the adapter after the models are setup
+
+        MovieAdapter adapter = new MovieAdapter(this, list);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     private void setUpModel(){
